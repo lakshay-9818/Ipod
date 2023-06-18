5dbbbbe5046d6dc2f3ddcbb43d39ddb38c729916
Lakshay Gupta <lakshaygupta3110@gmail.com>
2023-06-18 23:44:59 +0530

    final commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./eco-technology-145636.mp3";
+import music2 from "./floating.mp3";
+import music3 from "./futuristic.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
